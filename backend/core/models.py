5d67b2834a2dfa6a632fbeda1f7bd0d8b70a3c89
SardorDev12 <sardoribnfarhod@gmail.com>
2025-07-30 12:20:31 +0500

    created Project, Task models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subtasks')
+    attributes = models.JSONField(default=dict, blank=True)
+    responsibles = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.name
